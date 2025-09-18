9118db1408e186108aed778c3a999c4f8f322754
Diego <iwujigoodluck5@gmail.com>
2025-09-18 09:45:17 +0000

    adding vercel json file

--- dist/views/weatherView.js
+++
@@ -1,6 +0,0 @@
-export function renderWeather(message) {
-    const el = document.createElement("p");
-    el.textContent = `Weather says: ${message}`;
-    document.body.appendChild(el);
-}
-//# sourceMappingURL=weatherView.js.map
